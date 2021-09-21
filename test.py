a843f1fc546d475e6a65b2b013c53ed58a038d44
Khánh Hà Dương <bean17102000@gmail.com>
2021-09-21 19:47:19 +0700

    update

---
+++ test.py
@@ -0,0 +1,13 @@
+# from functools import total_ordering
+
+
+# taptin = open('C:/Users/ADMIN/Desktop/Python/Flask.py')
+# noidung = taptin.read()
+# taptin.close()
+# print(noidung)
+
+# with open('C:/Users/ADMIN/Desktop/Python/Flask.py') as tt:
+#     nd = tt.read()
+# print(nd)
+import os
+os.getcwd()
